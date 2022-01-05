3098490471e6f85d72a42ce0c92150cb9a8dfdf8
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-01-06 00:47:10 +0900

    V7
    다중 보안 클래스 추가

---
+++ basicSecurityV7/src/main/java/io/security/basicSecurity/BasicSecurityController.java
@@ -0,0 +1,40 @@
+package io.security.basicSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class BasicSecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+
+
+    }
+
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+
+    }
+    @GetMapping("/denined")
+    public String denined() {
+        return "denined";
+
+    }
+}
---
+++ basicSecurityV7/src/main/java/io/security/basicSecurity/SecurityConfig2.java
@@ -0,0 +1,15 @@
+package io.security.basicSecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception{
+
+
+        }
+    }
+
