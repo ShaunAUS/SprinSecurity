c97e9789d2f76dd4ffde4ec4e439c761d462d57f
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-01-20 23:17:19 +0900

    core v4
    회원가입 db저장, 패스워드 인코더

---
+++ core-spring-securityV4/src/main/java/corespringsecurity/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package corespringsecurity.repository;
+
+import corespringsecurity.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Account,Long> {
+}
